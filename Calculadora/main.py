34439f75ee0abf9070ebd72ccb1daedf0de2eec1
anheraco <anheraco@gmail.com>
2025-05-25 22:52:19 -0500

    "Agregada la función de división"

--- Calculadora/main.py
+++ Calculadora/main.py
@@ -9,7 +9,13 @@ def resta(a, b):
 def multiplicacion(a, b):
     return a * b
 
+def division(a, b):
+    if b == 0:
+        return "Error: división por cero"
+    return a / b
+
 if __name__ == "__main__":
     print("Suma:", suma(5, 3))
     print("Resta:", resta(5, 3))
     print("Multiplicación:", multiplicacion(5, 3))
+    print("División:", division(5, 0))
