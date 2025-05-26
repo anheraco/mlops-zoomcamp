9a9dc51752e8a5938b08c37e30c531484c960697
anheraco <anheraco@gmail.com>
2025-05-25 22:44:25 -0500

    "Calculadora con suma, resta y multiplicación"

---
+++ Calculadora/main.py
@@ -0,0 +1,15 @@
+# main.py
+
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+def multiplicacion(a, b):
+    return a * b
+
+if __name__ == "__main__":
+    print("Suma:", suma(5, 3))
+    print("Resta:", resta(5, 3))
+    print("Multiplicación:", multiplicacion(5, 3))
